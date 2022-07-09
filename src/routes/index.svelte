7ab5197903b782413f3bb364e4ed5302a107ee7d
Alexander Dorn <ad@not.one>
2022-07-09 23:55:59 +0200

    Made lib files from previous spurious routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
-  import Head from './head.svelte';
-  import Career from './career.svelte';
-  import Certification from './certification.svelte';
+  import Head from '$lib/head.svelte';
+  import Career from '$lib/career.svelte';
+  import Certification from '$lib/certification.svelte';
 
   let name = 'Alexander Dorn';
   let role = 'Software Artisan';
@@ -21,14 +21,3 @@
 
 <style>
 </style>
-
-<!--
-     <div class="hero min-h-screen bg-base-200">
-     <div class="hero-content text-center">
-     <div class="max-w-md">
-     <h1 class="text-5xl font-bold">Hello there</h1>
-     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-     <button class="btn btn-primary">Get Started</button>
-     </div>
-     </div>
-     </div> -->
