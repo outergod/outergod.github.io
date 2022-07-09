00aeb77e9d9b5d22b73c24750491b42812f39801
Alexander Dorn <ad@not.one>
2022-07-09 23:40:22 +0200

    Fix build issues

--- src/routes/avatar.svelte
+++ src/routes/avatar.svelte
@@ -4,6 +4,6 @@
 
 <div class="avatar">
   <div class="w-16 sm:w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 m-2">
-    <img src={avatar} alt="Picture of a handsome young man"/>
+    <img src={avatar} alt="Handsome young man"/>
   </div>
 </div>
